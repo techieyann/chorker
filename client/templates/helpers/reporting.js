08902f4ea3d9e6daa73953c57c6ef4edd5749fda
Ian McEachern <techieyann@gmail.com>
2014-12-06 22:59:02 -0800

    fixed limit

--- client/templates/helpers/reporting.js
+++ client/templates/helpers/reporting.js
@@ -1,10 +1,10 @@
 Template.recentChores.helpers({
 	chores: function () {
-		return Chores.find({},{sort: {last_completed: -1}}, {limit: 5});
+		return Chores.find({},{sort: {last_completed: -1}, limit: 5});
 	}
 });
 Template.leastRecentChores.helpers({
 	chores: function () {
-		return Chores.find({},{sort: {last_completed: 1}}, {limit: 5});
+		return Chores.find({},{sort: {last_completed: 1}, limit: 5});
 	}
 });
