17ebfcd0025cc5fe188eb7435a64d6114cd9cbc6
Ian McEachern <techieyann@gmail.com>
2014-12-08 16:10:37 -0800

    autofocus house pass

--- client/templates/helpers/userInit.js
+++ client/templates/helpers/userInit.js
@@ -1,5 +1,8 @@
 Template.userInit.events = {
 	'click .join-house': function (e) {
-		if (this) openModal('','joinHouseForm', '', this);
+		if (this) {
+			openModal('','joinHouseForm', '', this);
+			$('#pass').focus();
+		}
 	}
 };
