bbab15e4a9a1c693617488b840870a77a5001916
Ian McEachern <techieyann@gmail.com>
2015-01-30 16:13:35 -0800

    house doughnut chart init

--- client/templates/helpers/house.js
+++ client/templates/helpers/house.js
@@ -12,3 +12,34 @@ Template.house.helpers({
 		return false;
 	}
 });
+
+
+Template.houseByHousemate.rendered = function () {
+	var house = Session.get("house");
+	if (this && house) {
+
+		var housemateIds = Object.keys(house.members);
+
+		var chartData = [];
+		var colorsIndex = 0;
+		housemateIds.forEach(function (val) {
+			var total = Completed.find({user: val}).count();			
+
+			if (total) {
+				chartData.push({
+					value: total,
+					color: colors[colorsIndex].color,
+					highlight: colors[colorsIndex].highlight,
+					label: house.members[val]
+				});
+				colorsIndex++;
+				if(colorsIndex > colors.length) colorsIndex = 0;
+			}
+		});
+
+		// Get the context of the canvas element we want to select
+		var ctx = document.getElementById("house-doughnut-chart").getContext("2d");
+
+		new Chart(ctx).Doughnut(chartData,null);
+	}
+};
