f2e29ef773f4fb3a5f8c0791eda4e984ff27d7a1
Ian McEachern <techieyann@gmail.com>
2015-02-07 16:19:09 -0800

    modularized router controllers

--- client/js/router/controllers/base.js
+++ client/js/router/controllers/base.js
@@ -5,22 +5,3 @@ BaseController = RouteController.extend({
 	}
 });
 
-RegisteredController = BaseController.extend({
-	subscriptions: function () {
-		if (Meteor.user()) {
-			if (Meteor.user().profile.initialized) {
-				var houseId = Meteor.user().profile.house;
-				this.wait(Meteor.subscribe('completed', houseId));
-				this.wait(Meteor.subscribe('chores', houseId)); 
-				this.wait(Meteor.subscribe('myHouse', houseId));
-				if(this.ready()) {
-					Session.set("house", Houses.findOne());
-					this.render();
-				}
-			}
-		}
-		this.render('loading');
-
-
-	}
-});
